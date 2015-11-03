2a950a11bfa5485aeaa2c3ddd3a48fea89fe7f4d
Mike Grabowski <grabbou@gmail.com>
2015-11-03 16:08:49 +0100

    Add nicer check-updates

--- Readme.md
+++ Readme.md
@@ -2,3 +2,12 @@ react-universal
 ===============
 
 Sample demonstration of universal multi-target app. See `docs/` for more details.
+
+## Installing
+
+```bash
+$ npm install
+$ npm run install:<platform>
+```
+
+> npm install automatically installs the umbrella & core for you.
--- scripts/check-updates.js
+++ scripts/check-updates.js
@@ -1,7 +1,9 @@
 import * as utils from './utils';
 
 export default function main({rootPath, flags}) {
-  const folders = ['core', ...utils.getAllPlatforms()];
-
-  return folders.map(path => utils.runInFolder(rootPath, path, `npm-check-updates ${flags[0]}`));
+  const folders = ['.', 'core', ...utils.getAllPlatforms()];
+  const command = `npm-check-updates`;
+  return folders.map(path => utils.runInFolder(rootPath, path, flags
+    ? command + ` ${flags.join(' ')}`
+    : command));
 }
